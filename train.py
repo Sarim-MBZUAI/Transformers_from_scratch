26498f2d89afacdfa5eb574978ddc72c5846b15c
Sarim-MBZUAI <142799922+Sarim-MBZUAI@users.noreply.github.com>
2024-07-16 23:39:27 +0400

    Update train.py

--- train.py
+++ train.py
@@ -1,11 +1,4 @@
-# import torch
-# import torch.nn as nn
 
-# from datasets import load_dataset
-# from tokenizers import tokenizers
-
-# from tokenizers.model import WordLevel
-# from tokenizers.
 
 
 from model import build_transformer
